ea41079e74bec89c651ef2f3dea35f4be6e68da6
Lukas lubingo <78145529+lukaby-eric@users.noreply.github.com>
2023-07-27 09:35:02 -0500

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+var mainImg= document.getElementById("main-img");
+var mainName= document.getElementById("main-name");
+var mainTitle= document.getElementById("main-title");
+
+function nick() {
+    mainImg.src="../images/Nick-2-1-819x1024.jpg";
+    mainName.innerHTML="Nick Cody";
+}
+function julia() {
+    mainImg.src="../images/Julia-1229x1536.jpg";
+    mainName.innerHTML="Julia Davies";
+}
+function johnny() {
+    mainImg.src="../images/Johnny-1229x1536.jpg";
+    mainName.innerHTML="John Donnelly";
+}
+function lin() {
+    mainImg.src="../images/Lin-En-1229x1536.jpg";
+    mainName.innerHTML="Lin-En Chen";
+}
+function trinity() {
+    mainImg.src="../images/Trinity-1229x1536.jpg";
+    mainName.innerHTML="Trinity Christopher";
+}
+function dajung() {
+    mainImg.src="../images/Dajung-1-819x1024.jpg";
+    mainName.innerHTML="Dajung Miller";
+}
+function brendan() {
+    mainImg.src="../images/Brendan-2-1-819x1024.jpg";
+    mainName.innerHTML="Brendan Crews";
+}
